a1f30163773796720f8bdec86093f8d859e1cca0
rathin <rathin2104@gmail.com>
2023-04-13 10:34:57 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
+import "../styles/App.css"
+import React, { useState } from 'react';
+import Tabs from "./Tabs"
 
+function App(){
+
+  //const tabNames = ["Home","About", "Features"]
+  const [tabNames,setTabNames] = useState(["Home","About", "Features"])
+
+    return (
+      <div className='app'>
+          <div className='browser'>
+            <Tabs tabNames={tabNames}/>
+            <div className="viewport">Pages Go Here</div>
+          </div> 
+      </div>
+    );
+}
+
+export default App;
